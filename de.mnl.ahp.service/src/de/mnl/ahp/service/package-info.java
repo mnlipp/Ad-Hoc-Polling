0380980dfac2df8eb81867da97d8fd3dbb26b3f8
Michael N. Lipp <mnl@mnl.de>
2021-02-22 17:42:32 +0100

    Add some versioning.

--- de.mnl.ahp.service/src/de/mnl/ahp/service/package-info.java
+++ de.mnl.ahp.service/src/de/mnl/ahp/service/package-info.java
@@ -16,5 +16,5 @@
  * with this program; if not, see <http://www.gnu.org/licenses/>.
  */
 
-@org.osgi.annotation.versioning.Version("0.0.0")
+@org.osgi.annotation.versioning.Version("${api_version}")
 package de.mnl.ahp.service;
